021a39e1981cc3ded1e9c7a4de15db5081ebe2b2
ak1ra <git@ak1ra.xyz>
2023-10-22 15:10:06 +0000

    cmdline: fix bin/pixiv-to-gif.sh

--- cmdline/bin/pixiv-to-gif.sh
+++ cmdline/bin/pixiv-to-gif.sh
@@ -1,29 +1,32 @@
-#! /bin/bash
+#!/bin/bash
+# Convert all pxder downloaded Pixiv .zip archive to .gif/.mp4
+# ref: https://github.com/Tsuk1ko/pxder
 
-function pixiv2gif() {
+pixiv_to_gif() {
     infile="$1"
-    outfile_format="$2"
+    format="$2"
     delay="$3"
-    infile_dir=$(dirname "$infile")
-    infile_basename=$(basename "$infile")
-    exdir="${infile_basename%.*}"
 
-    test -n "$delay" || delay="$(echo "${exdir#*@}" | sed 's/ms//')"
-    outfile="${exdir%@*}@${delay}ms.${outfile_format}"
-    framerate="$(echo 'scale=3;1000/'"$delay"'' | bc)"
+    infile_realpath="$(realpath -s "$infile")"
+    extract_dir="${infile_realpath%.zip}"
 
-    cd "$infile_dir" || return
-    if [ ! -f "$outfile" ]; then
-        echo "pixiv2gif '$infile' '$infile_dir/$outfile'"
-        unzip -q -o -d "$exdir" "$infile_basename"
+    # infile="(46671388)miku 深海少女@80ms.zip"
+    test -n "$delay" || delay="$(echo "$infile" | sed -E 's%.*@([0-9]+)ms\.zip$%\1%')"
+    outfile="${infile_realpath%@*}@${delay}ms.${format}"
+    framerate="$(echo "scale=3;1000/${delay}" | bc)"
 
-        cd "$exdir" || return
-        ffmpeg -y -framerate "$framerate" -i "%06d.jpg" "../$outfile" 2>/dev/null
-        cd ../ || return
+    cd "$(dirname "$infile_realpath")" || return
+    test -f "$outfile" || {
+        echo "convert '$infile_realpath' '$outfile'"
+        unzip -q -o -d "$extract_dir" "$infile_realpath"
 
-        rm -rf "$exdir"
-    fi
-    cd ../ || return
+        cd "$extract_dir" || return
+        ffmpeg -y -framerate "$framerate" -i "%06d.jpg" "$outfile" 2>/dev/null
+        cd .. || return
+
+        rm -rf "$extract_dir"
+    }
+    cd .. || return
 }
 
 main() {
@@ -34,12 +37,12 @@ main() {
 
     require_command ffmpeg unzip parallel
 
-    outfile_format="$1"
-    test -n "$outfile_format" || outfile_format=mp4
+    format="$1"
+    test -n "$format" || format=mp4
 
-    export -f pixiv2gif
+    export -f pixiv_to_gif
     find . -type f -name '*.zip' -print0 |
-        parallel -0 pixiv2gif {} "$outfile_format"
+        parallel -0 pixiv_to_gif {} "$format"
 }
 
 main "$@"
