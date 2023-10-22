29a33087ac7426998aba6e0f524817058346e61b
ak1ra <git@ak1ra.xyz>
2023-10-22 13:03:32 +0000

    bash: make debx.sh a function

--- bash/bin/debx.sh
+++ bash/bin/debx.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# SC3045 (error): In dash, read -d is not supported.
-find . -maxdepth 1 -type f -name '*.deb' -print0 | while IFS= read -r -d '' file; do
-    realpath="$(realpath -s "$file")"
-    test -d "${realpath%.deb}" || {
-        mkdir -p "${realpath%.deb}"
-        cd "${realpath%.deb}" || return
-        ar x "$realpath"
-        mkdir control data
-        tar -xf control.tar.xz -C control
-        tar -xf data.tar.xz -C data
-        cd "$(dirname "$realpath")" || return
-    }
-done
+debx() {
+    # [SC2044 – For loops over find output are fragile](https://www.shellcheck.net/wiki/SC2044)
+    # [SC3045 (error): In dash, read -d is not supported](https://www.shellcheck.net/wiki/SC3045)
+    find . -maxdepth 1 -type f -name '*.deb' -print0 | while IFS= read -r -d '' file; do
+        realpath="$(realpath -s "$file")"
+        test -d "${realpath%.deb}" || {
+            mkdir -p "${realpath%.deb}"
+            cd "${realpath%.deb}" || return
+            ar x "$realpath"
+            mkdir control data
+            tar -xf control.tar.xz -C control
+            tar -xf data.tar.xz -C data
+            cd "$(dirname "$realpath")" || return
+        }
+    done
+}
+
+debx "$@"
