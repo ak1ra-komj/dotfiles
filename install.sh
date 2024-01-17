25a25fd927ebbf8bc972a2430bb714abb7b9057f
ak1ra <git@ak1ra.xyz>
2024-01-17 16:32:16 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,32 @@
 #! /bin/bash
 
-test -d ~/bin || mkdir -p ~/bin
+stow_bash() {
+    test -L ~/.bashrc || cp -v ~/.bashrc ~/.bashrc.$(date +%F_%s)
+    test -L ~/.profile || cp -v ~/.profile ~/.profile.$(date +%F_%s)
+    stow bash
+}
 
-for p in ansible bash dig git tmux vim wget; do
-    stow $p
-done
+stow_cmdline() {
+    test -d ~/bin || mkdir ~/bin
+    stow cmdline
+}
+
+stow_ansible() {
+    test -d ~/.ansible || mkdir -p ~/.ansible
+    stow ansible
+}
+
+main() {
+    stow_bash
+    stow_cmdline
+    # stow_ansible
+
+    stow git
+    stow vim
+
+    # stow tmux
+    # stow dig
+    # stow wget
+}
+
+main "$@"
