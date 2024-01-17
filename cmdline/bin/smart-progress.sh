566792e03ef2f7c92b6c76a599ecbbd4dd572160
ak1ra <git@ak1ra.xyz>
2024-01-17 15:44:26 +0800

    cmdline: add smart-progress.sh

---
+++ cmdline/bin/smart-progress.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+mapfile -t blockdevices < <(lsblk --output=path --scsi --json | jq -r .blockdevices[].path | sort)
+
+selftest_log() {
+    for dev in "${blockdevices[@]}"; do
+        echo "========== ${dev} =========="
+        smartctl --log=selftest "${dev}"
+    done
+}
+
+selftest_status() {
+    echo "========== .ata_smart_data.self_test.status =========="
+    for dev in "${blockdevices[@]}"; do
+        printf "%s\t%s\n" "${dev}" "$(smartctl -j -c "${dev}" | jq -r .ata_smart_data.self_test.status.string)"
+    done
+}
+
+selftest_log
+selftest_status
