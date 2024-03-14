8bc4c388836973222f04adb7a4034d5e24e5d57a
ak1ra <git@ak1ra.xyz>
2024-03-14 15:54:53 +0800

    add date-prefix-rename.sh

---
+++ cmdline/bin/date-prefix-rename.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+main() {
+    mapfile -t src_files < <(
+        find "$1" -type f | xargs realpath --relative-to="$(pwd)" -s | grep -E '\.md$'
+    )
+
+    for src in "${src_files[@]}"; do
+        dirname="${src%/*}"
+        basename="${src##*/}"
+
+        date_prefix_regex="^(([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{2})-)?"
+        # stat -c --format, %X - access, %Y - modify, %W - birth, %Z - status change
+        date_prefix="$(date --date="@$(stat --format='%Y' "${src}")" +%F)-"
+        dest="${dirname}/$(echo "${basename}" | sed -E 's%'"${date_prefix_regex}"'%'"${date_prefix}"'%')"
+
+        echo mv -v "${src}" "${dest}"
+    done
+}
+
+main "$@"
