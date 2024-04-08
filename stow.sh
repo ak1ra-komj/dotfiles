4b08f4de2870d1772a45be9df2e3d84b212efd93
ak1ra <git@ak1ra.xyz>
2024-04-08 12:44:35 +0800

    update stow.sh (rename from install.sh)

---
+++ stow.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+require_command() {
+    for c in "$@"; do
+        command -v "$c" >/dev/null || {
+            echo >&2 "required command '$c' is not installed, aborting..."
+            exit 1
+        }
+    done
+}
+
+stow_keep() {
+    keep_dir="$1"
+    test -d "${keep_dir}" || mkdir -p "${keep_dir}"
+    touch "${keep_dir}/.stow_keep"
+}
+
+main() {
+    require_command stow
+
+    stow_keep "${HOME}/bin"
+    test -L "${HOME}/.bashrc" || mv -v "${HOME}/.bashrc" "${HOME}/.bashrc.$(date +%F_%s)"
+    test -L "${HOME}/.profile" || mv -v "${HOME}/.profile" "${HOME}/.profile.$(date +%F_%s)"
+    stow bash
+    stow cmdline
+
+    stow git
+    stow vim
+
+    stow_keep "${HOME}/.ansible/inventory"
+    stow ansible
+
+    stow dig
+    # stow wget
+    # stow tmux
+}
+
+main "$@"
