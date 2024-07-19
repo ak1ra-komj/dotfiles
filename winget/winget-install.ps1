6529247481afc291da1304bad795114cf2a50a35
ak1ra <git@ak1ra.xyz>
2024-07-19 18:05:22 +0800

    winget: fix foreach

--- winget/winget-install.ps1
+++ winget/winget-install.ps1
@@ -1,7 +1,8 @@
 
 $packages_url = "https://raw.githubusercontent.com/ak1ra-komj/dotfiles/master/winget/winget-packages.txt"
+$packages = (Invoke-RestMethod $packages_url) -split "`n"
 
-foreach ($package in $(Invoke-RestMethod $packages_url)) {
+foreach ($package in $packages) {
     Write-Host ('Installing package {0}...' -f $package) -ForegroundColor Green
     winget install --id=$package --exact --accept-package-agreements --accept-source-agreements
 }
