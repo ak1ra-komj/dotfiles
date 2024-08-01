d526a8ffa8ecb49c9d34a7a821b6d13b60e9c236
ak1ra <git@ak1ra.xyz>
2024-08-01 11:16:45 +0800

    update ~/bin/urlencode.py

--- cmdline/bin/urlencode.py
+++ cmdline/bin/urlencode.py
@@ -6,23 +6,23 @@ import argparse
 from urllib.parse import quote
 
 
-def urlencode(urls):
-    for url in urls:
-        print(quote(url))
+def urlencode(texts):
+    for text in texts:
+        print(quote(text))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "urls",
+        "texts",
         nargs="+",
-        metavar="URL",
-        help="urls to encode with urllib.parse.quote()",
+        metavar="TEXT",
+        help="texts to encode with urllib.parse.quote()",
     )
     args = parser.parse_args()
 
-    if len(args.urls) > 0:
-        urlencode(args.urls)
+    if len(args.texts) > 0:
+        urlencode(args.texts)
 
 
 if __name__ == "__main__":
