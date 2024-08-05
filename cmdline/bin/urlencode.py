611ec0acb6b6c4c151081083adec4f7dedd8be3a
ak1ra <git@ak1ra.xyz>
2024-08-05 17:57:35 +0800

    urlencode.py: use fileinput

--- cmdline/bin/urlencode.py
+++ cmdline/bin/urlencode.py
@@ -1,29 +1,10 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-import argparse
-
-from urllib.parse import quote
-
-
-def urlencode(texts):
-    for text in texts:
-        print(quote(text))
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "texts",
-        nargs="+",
-        metavar="TEXT",
-        help="texts to encode with urllib.parse.quote()",
-    )
-    args = parser.parse_args()
-
-    if len(args.texts) > 0:
-        urlencode(args.texts)
+import fileinput
+import urllib.parse
 
 
 if __name__ == "__main__":
-    main()
+    for line in fileinput.input():
+        print(urllib.parse.quote(line.strip()))
