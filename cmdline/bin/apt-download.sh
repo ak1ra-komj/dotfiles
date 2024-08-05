07e64388317dc35d27b06342f4c3604b9b77e343
ak1ra <git@ak1ra.xyz>
2024-08-05 12:43:54 +0800

    add ~/bin/apt-download.sh

---
+++ cmdline/bin/apt-download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# How to deal with virtual-packages and meta-packages?
+
+apt_download() {
+    apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts \
+        --no-breaks --no-replaces --no-enhances "$@" | grep "^\w" | xargs apt-get download
+}
+
+apt_download "$@"
