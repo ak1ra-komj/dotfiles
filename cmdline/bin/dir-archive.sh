6efc7a2c00eda86771e93d5b21af69aa158c06fe
ak1ra <git@ak1ra.xyz>
2024-08-19 15:47:40 +0800

    cmdline: add --help option for dir-archive.sh

--- cmdline/bin/dir-archive.sh
+++ cmdline/bin/dir-archive.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 # date: 2024-08-19
 # author: ak1ra
-# create archives by directory, just pipe the output to sh
-# dir-archive.sh | sh
+# create archives by directory
+
+usage() {
+    cat <<EOF
+Usage:
+    dir-archive.sh [7z|zip|tar]
+
+Examples:
+    Show archive commands:
+        dir-archive.sh 7z
+
+    The output is manually passed to 'sh' to be executed sequentially:
+        dir-archive.sh 7z | sh
+
+EOF
+    exit 0
+}
 
 dir_archive() {
     case "$1" in
     7z)
-        archive_cmd="$(command -v 7z) a -- '%s.7z' '%s'"
+        # 7zip has builin multiprocessing
+        archive_cmd="$(command -v 7z) a -r -- '%s.7z' '%s'"
         ;;
     zip)
         archive_cmd="$(command -v zip) -r '%s.zip' -- '%s'"
@@ -21,5 +37,9 @@ dir_archive() {
         LANG=C.UTF-8 sort |
         awk '!/\.(\.|git)?$/ {printf("'"${archive_cmd}"'\n", $0, $0)}'
 }
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    usage
+fi
 
 dir_archive "$@"
