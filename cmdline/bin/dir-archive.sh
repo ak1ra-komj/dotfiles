f7143f088e836305524f746bac2099c92896175c
ak1ra <git@ak1ra.xyz>
2024-08-19 12:39:48 +0800

    update ~/bin/dir-archive.sh

--- cmdline/bin/dir-archive.sh
+++ cmdline/bin/dir-archive.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # date: 2024-08-19
 # author: ak1ra
-# create archives by directory
+# create archives by directory, just pipe the output to sh
+# dir-archive.sh | sh
 
 dir_archive() {
     case "$1" in
     7z)
-        archive_cmd="7z a -- '%s.7z' '%s'"
+        archive_cmd="$(command -v 7z) a -- '%s.7z' '%s'"
         ;;
     zip)
-        archive_cmd="zip -r '%s.zip' '%s'"
+        archive_cmd="$(command -v zip) -r '%s.zip' -- '%s'"
         ;;
     *)
-        archive_cmd="tar -cf '%s.tar' '%s'"
+        archive_cmd="$(command -v tar) -cf '%s.tar' -- '%s'"
         ;;
     esac
 
