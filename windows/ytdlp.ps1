d94a290066148c5c453f30ee79d422a72a1f5387
ak1ra <git@ak1ra.xyz>
2024-09-04 11:33:50 +0800

    update aria2cArgs in windows/ytdlp.ps1

--- windows/ytdlp.ps1
+++ windows/ytdlp.ps1
@@ -4,7 +4,7 @@
 # https://github.com/ak1ra-komj/dotfiles/blob/master/windows/ytdlp.ps1
 
 param(
-    [Parameter(ValueFromRemainingArguments=$true)]
+    [Parameter(ValueFromRemainingArguments = $true)]
     $args
 )
 
@@ -14,11 +14,16 @@ $aria2cPath = (Get-Command aria2c).Source
 
 # Determine the path to the CA certificate relative to the aria2c executable
 $caCertificatePath = (Join-Path (Split-Path $aria2cPath) "ca-certificates.crt") -replace '\\', '/'
+$aria2cArgs = (
+    "--ca-certificate=$caCertificatePath",
+    "--max-concurrent-downloads=8",
+    "--max-connection-per-server=4"
+) -join ' '
 
 # Execute yt-dlp with additional parameters
 # Try after closing chrome completely OR Launch chrome.exe with the flag: --disable-features=LockProfileCookieDatabase
 # Ref: https://github.com/yt-dlp/yt-dlp/issues/7271#issuecomment-1584404779
 & $ytDlpPath --cookies-from-browser=chrome `
-             --downloader=aria2c `
-             --downloader-args="aria2c:--max-concurrent-downloads=8 --max-connection-per-server=4 --ca-certificate=$caCertificatePath" `
-             @args
+    --downloader=aria2c `
+    --downloader-args="aria2c:$aria2cArgs" `
+    @args
