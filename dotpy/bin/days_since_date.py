eb8f90568e6ebd09aa5fa16f4be9f1c87789e40f
ak1ra <git@ak1ra.xyz>
2025-01-24 00:28:54 +0800

    split function on dotpy/bin/days_since_date.py

--- dotpy/bin/days_since_date.py
+++ dotpy/bin/days_since_date.py
@@ -16,6 +16,67 @@ from icalendar import (
 )
 
 
+def get_utc_event_dtstart(
+    startdate: datetime.date,
+    event_time: datetime.time,
+    timezone: zoneinfo.ZoneInfo,
+    days: int,
+):
+    # Google Calendar 似乎"强制"以 UTC 时间存储 DTSTART 和 DTEND
+    local_datetime = datetime.datetime.combine(
+        date=startdate + datetime.timedelta(days=days),
+        time=event_time,
+        tzinfo=timezone,
+    )
+    # 将 local_datetime "强制"转换为 UTC 时间
+    utc = zoneinfo.ZoneInfo("UTC")
+    dtstart = local_datetime.replace(tzinfo=utc) - local_datetime.utcoffset()
+
+    return dtstart
+
+
+def add_days_event_to_calendar(
+    calendar: Calendar,
+    startdate: datetime.date,
+    timezone: zoneinfo.ZoneInfo,
+    reminders: list[int],
+    attendees: list[str],
+    days: int,
+):
+    # 设定 DTSTART 为 当地时间 的 09:00
+    event_time = datetime.time(hour=9, minute=0)
+    # 设定 VEVENT 的时长为 1 小时
+    event_duration = datetime.timedelta(hours=1)
+
+    age = round(days / 365.25, 2)
+    summary = f"{days} days since {startdate.isoformat()} (age: {age})"
+    dtstart = get_utc_event_dtstart(startdate, event_time, timezone, days)
+    dtend = dtstart + event_duration
+
+    event = Event()
+    event.add("summary", summary)
+    event.add("dtstart", vDatetime(dtstart))
+    event.add("dtend", vDatetime(dtend))
+
+    # 添加提醒
+    for reminder_days in reminders:
+        alarm = Alarm()
+        trigger_time = datetime.timedelta(days=-reminder_days)
+        alarm.add("action", "DISPLAY")
+        alarm.add("description", f"Reminder: {summary}")
+        alarm.add("trigger", trigger_time)
+        event.add_component(alarm)
+
+    # 添加与会者
+    for attendee_email in attendees:
+        attendee = vCalAddress(f"mailto:{attendee_email}")
+        attendee.params["cn"] = vText(attendee_email.split("@")[0])
+        attendee.params["role"] = vText("REQ-PARTICIPANT")
+        event.add("attendee", attendee)
+
+    calendar.add_component(event)
+
+
 def days_since_date(
     startdate: datetime.date,
     timezone: zoneinfo.ZoneInfo,
@@ -25,58 +86,28 @@ def days_since_date(
     max_days: int,
     output: Path,
 ):
-    cal = Calendar()
-    calname = f"days since {startdate.isoformat()}"
-    cal.add("PRODID", "-//Google Inc//Google Calendar//EN")
-    cal.add("VERSION", "2.0")
-    cal.add("CALSCALE", "GREGORIAN")
-    cal.add("X-WR-CALNAME", calname)
-    cal.add("X-WR-TIMEZONE", timezone)
+    calendar = Calendar()
+    calendar.add("PRODID", "-//Google Inc//Google Calendar//EN")
+    calendar.add("VERSION", "2.0")
+    calendar.add("CALSCALE", "GREGORIAN")
+    calendar.add("X-WR-CALNAME", f"days since {startdate.isoformat()}")
+    calendar.add("X-WR-TIMEZONE", timezone)
 
-    # Google Calendar 似乎"强制"以 UTC 时间存储 DTSTART 和 DTEND
-    # 转换为当地时间时使用全局的 X-WR-TIMEZONE
-    utc = zoneinfo.ZoneInfo("UTC")
-    for days in range(interval, max_days, interval):
-        age = round(days / 365.25, 2)
-        # 设定 DTSTART 为 当地时间 的 09:00
-        localtime = datetime.datetime.combine(
-            date=startdate + datetime.timedelta(days=days),
-            time=datetime.time(9, 0),
-            tzinfo=timezone,
+    for days in range(interval, max_days + 1, interval):
+        add_days_event_to_calendar(
+            calendar=calendar,
+            startdate=startdate,
+            timezone=timezone,
+            reminders=reminders,
+            attendees=attendees,
+            days=days,
         )
-        # 将 localtime "强制"转换为 UTC 时间
-        dtstart = localtime.replace(tzinfo=utc) - localtime.utcoffset()
-        dtend = dtstart + datetime.timedelta(hours=1)
-        summary = f"{days} days since {startdate.isoformat()} (age: {age})"
-
-        event = Event()
-        event.add("summary", summary)
-        event.add("dtstart", vDatetime(dtstart))
-        event.add("dtend", vDatetime(dtend))
-
-        # 添加提醒
-        for reminder_days in reminders:
-            alarm = Alarm()
-            trigger_time = datetime.timedelta(days=-reminder_days)
-            alarm.add("action", "DISPLAY")
-            alarm.add("description", f"Reminder: {summary}")
-            alarm.add("trigger", trigger_time)
-            event.add_component(alarm)
-
-        # 添加与会者
-        for attendee_email in attendees:
-            attendee = vCalAddress(f"mailto:{attendee_email}")
-            attendee.params["cn"] = vText(attendee_email.split("@")[0])
-            attendee.params["role"] = vText("REQ-PARTICIPANT")
-            event.add("attendee", attendee)
-
-        cal.add_component(event)
 
     if not output:
         output = Path(f"days_since_date.{startdate.isoformat()}.ics")
 
     with output.open("wb") as f:
-        f.write(cal.to_ical())
+        f.write(calendar.to_ical())
     print(f"iCal file saved to {output}")
 
 
@@ -121,7 +152,7 @@ def main():
         "-d",
         "--max-days",
         type=int,
-        default=31000,
+        default=30000,
         help="Max days to generate events (default: %(default)s).",
     )
     parser.add_argument(
