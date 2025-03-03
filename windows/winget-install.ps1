320e9d39512c5a8cd025927e5aaff723972922f8
ak1ra <git@ak1ra.xyz>
2025-03-03 11:19:27 +0800

    group windows/winget-packages.txt

--- windows/winget-install.ps1
+++ windows/winget-install.ps1
@@ -1,8 +1,16 @@
 
-$packages_url = "https://raw.githubusercontent.com/ak1ra-komj/dotfiles/master/windows/winget-packages.txt"
+param (
+    [string]$packages_url = "https://raw.githubusercontent.com/ak1ra-komj/dotfiles/master/windows/winget-packages.txt"
+)
+
 $packages = (Invoke-RestMethod $packages_url) -split "`n"
 
 foreach ($package in $packages) {
+    $package = $package.Trim()
+    if ($package -eq "" -or $package -match "^#") {
+        continue
+    }
+
     Write-Host ('Installing package {0}...' -f $package) -ForegroundColor Green
     winget install --id=$package --exact --accept-package-agreements --accept-source-agreements
 }
