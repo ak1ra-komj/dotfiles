672ae91ec10d7fadaf672abb90da03b9089c4356
ak1ra <git@ak1ra.xyz>
2025-04-11 16:46:07 +0800

    add nvme support on wwn-ata-mapping.sh

--- dotsh/bin/wwn-ata-mapping.sh
+++ dotsh/bin/wwn-ata-mapping.sh
@@ -2,33 +2,46 @@
 # author: ak1ra
 # date: 2025-04-11
 
-# Create temp maps for ata and wwn
-declare -A dev_ata_map
+map_ids() {
+	# declare -n, making it a name reference to another variable.
+	# shellcheck disable=SC2034
+	local -n nameref_map=$1
+	local find_pattern="$2"
+	local grep_exclude="$3"
+
+	while IFS= read -r link; do
+		dev=$(readlink -f "$link")
+		dev_basename=$(basename "$dev")
+		link_basename=$(basename "$link")
+		# shellcheck disable=SC2034
+		nameref_map["$dev_basename"]="$link_basename"
+	done < <(
+		find /dev/disk/by-id -type l \
+			-regextype posix-extended -regex "$find_pattern" |
+			grep -vE -- '-part[0-9]+$' | {
+			if [[ -n "$grep_exclude" ]]; then
+				grep -vE -- "$grep_exclude"
+			else
+				cat
+			fi
+		}
+	)
+}
+
+# declare -A dev_eui_map
+# declare -A dev_nvme_map
+# map_ids dev_eui_map "/dev/disk/by-id/nvme-eui\..*" ""
+# map_ids dev_nvme_map "/dev/disk/by-id/nvme-.*" "nvme-eui\."
+
+# for dev in "${!dev_eui_map[@]}"; do
+# 	printf "%s\t%s\n" "${dev_eui_map[$dev]}" "${dev_nvme_map[$dev]:-(no nvme-ID found)}"
+# done
+
 declare -A dev_wwn_map
+declare -A dev_ata_map
+map_ids dev_wwn_map "/dev/disk/by-id/wwn-.*" ""
+map_ids dev_ata_map "/dev/disk/by-id/(ata|scsi)-.*" ""
 
-# Populate ATA map
-while IFS= read -r link; do
-    dev=$(readlink -f "$link")
-    dev_name=$(basename "$dev")
-    ata_name=$(basename "$link")
-    dev_ata_map["$dev_name"]="$ata_name"
-done < <(find /dev/disk/by-id/ -type l -name 'ata-*' ! -name '*-part*' | sort)
-
-# Populate WWN map
-while IFS= read -r link; do
-    dev=$(readlink -f "$link")
-    dev_name=$(basename "$dev")
-    wwn_name=$(basename "$link")
-    dev_wwn_map["$dev_name"]="$wwn_name"
-done < <(find /dev/disk/by-id/ -type l -name 'wwn-*' ! -name '*-part*' | sort)
-
-# Print mapping
 for dev in "${!dev_wwn_map[@]}"; do
-    ata="${dev_ata_map[$dev]}"
-    wwn="${dev_wwn_map[$dev]}"
-    if [[ -n "${ata}" ]]; then
-        printf "%s\t%s\n" "${wwn}" "${ata}"
-    else
-        printf "%s\t(no ata-ID found)\n" "${wwn}"
-    fi
-done | sort -k2
+	printf "%s\t%s\n" "${dev_wwn_map[$dev]}" "${dev_ata_map[$dev]:-(no ata-ID found)}"
+done
