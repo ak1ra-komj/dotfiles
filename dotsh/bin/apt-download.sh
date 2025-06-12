bd848232c379459b7e3a5800ba69baa26c50dc3f
ak1ra <git@ak1ra.xyz>
2025-06-12 12:10:20 +0800

    update dotsh/bin/apt-download.sh

--- dotsh/bin/apt-download.sh
+++ dotsh/bin/apt-download.sh
@@ -2,8 +2,15 @@
 # How to deal with virtual-packages and meta-packages?
 
 apt_download() {
-    apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts \
-        --no-breaks --no-replaces --no-enhances "$@" | grep "^\w" | xargs apt-get download
+    apt-cache depends \
+        --recurse \
+        --no-recommends \
+        --no-suggests \
+        --no-conflicts \
+        --no-breaks \
+        --no-replaces \
+        --no-enhances "$@" |
+        grep "^\w" | xargs apt-get download
 }
 
 apt_download "$@"
